5be97415c22d4c0aed856363ceb0dcb29eb91082
Quỳnh Như <nhutran220205@gmail.com>
2024-04-20 14:42:17 +0700

    lien ket co so du lieu

--- src/main/java/com/example/quanlynhanvien/GiaoDienDangKyController.java
+++ src/main/java/com/example/quanlynhanvien/GiaoDienDangKyController.java
@@ -1,9 +1,12 @@
 package com.example.quanlynhanvien;
 
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 
 import java.net.URL;
@@ -16,12 +19,25 @@ public class GiaoDienDangKyController implements Initializable {
     private TextField tf_mail;
 
     @FXML
-    private TextField tf_password;
+    private PasswordField tf_password;
     @FXML
     private Button bt_signup;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+        bt_signup.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                String tendangnhap = tf_fullname.getText();
+                String Email = tf_mail.getText();
+                String password = tf_password.getText();
+                if (tendangnhap != "" && Email != "" && password != ""){
+                    
+                }
 
+
+
+            }
+        });
     }
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.example.quanlynhanvien {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.sql;
 
 
     opens com.example.quanlynhanvien to javafx.fxml;
